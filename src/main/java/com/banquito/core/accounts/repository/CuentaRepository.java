ec8430b518c752e0eaa67e7f8685bd028bd45a4e
Mathius Moyano <msmj1077@gmail.com>
2026-05-17 11:03:54 -0500

    Agregado cuenta-favorita-pagos

--- src/main/java/com/banquito/core/accounts/repository/CuentaRepository.java
+++ src/main/java/com/banquito/core/accounts/repository/CuentaRepository.java
@@ -1,12 +1,15 @@
 package com.banquito.core.accounts.repository;
 
-import com.banquito.core.accounts.model.Cuenta;
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.banquito.core.accounts.model.Cuenta;
 
 public interface CuentaRepository extends JpaRepository<Cuenta, Integer> {
 
     Optional<Cuenta> findByNumeroCuenta(String numeroCuenta);
     List<Cuenta> findByClienteId(Integer clienteId);
+    Optional<Cuenta> findFirstByClienteIdAndEsFavoritaPagosTrue(Integer clienteId);
 }
