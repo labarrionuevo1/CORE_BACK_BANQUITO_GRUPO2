ade30e450368f2a969bf647bbe65689b8ec689af
Cams <117851597+CamilaParedes23@users.noreply.github.com>
2026-05-16 22:00:07 -0500

    Modificaciones para la parte de Clientes

--- src/main/java/com/banquito/core/customers/repository/ClienteRepository.java
+++ src/main/java/com/banquito/core/customers/repository/ClienteRepository.java
@@ -7,10 +7,14 @@ import com.banquito.core.customers.enums.TipoIdentificacionEnum;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
     Optional<Cliente> findByTipoIdentificacionAndIdentificacion(TipoIdentificacionEnum tipoIdentificacion, String identificacion);
     Optional<Cliente> findByIdentificacion(String identificacion);
     List<Cliente> findByTipoClienteAndActivoPagosMasivosAndEstado(TipoClienteEnum tipoCliente, Boolean activoPagosMasivos, EstadoClienteEnum estado);
+
+    @Query("SELECT c FROM Cliente c LEFT JOIN FETCH c.representanteLegal WHERE c.id = :id")
+    Optional<Cliente> findByIdWithRepresentanteLegal(Integer id);
 }
